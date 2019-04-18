9c7f0769e8a3258a361404b7faa501d8ba31914b
Bono Opalfvens <bono-opalfvens@hotmail.com>
2019-04-18 13:35:55 +0200

    [Feat] Add basic toolbar + install normalize.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
     // AngularMaterialAnimations
     BrowserAnimationsModule,
     // AngularMaterial
-    MatButtonModule, MatCheckboxModule
+    MatToolbarModule,
+    MatMenuModule, 
+    MatButtonModule,
+    MatIconModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
