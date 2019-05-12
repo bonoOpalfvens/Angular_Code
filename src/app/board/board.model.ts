4092c93bdb323f749bffe190e10fd2173f3df577
Bono Opalfvens <bono.opalfvens@student.hogent.be>
2019-05-12 21:18:56 +0200

    [Feat] Update models

--- src/app/board/board.model.ts
+++ src/app/board/board.model.ts
@@ -1,29 +1,40 @@
 import { Post } from '../post/post.model';
-import { environment } from 'src/environments/environment';
 
 export class Board {
     constructor(
-        private _id: Number,
+        private _id: number,
         private _name: string,
+        private _description: string,
         private _icon: string,
-        private _posts = new Array<Post>()
+        private _posts = new Array<Post>(),
+        private _likes: number,
+        private _isLiking: boolean
     ){}
 
     static fromJSON(json: any): Board {
-        const obj = new Board(json.id, json.name, json.icon, json.posts);
+        const obj = new Board(json.id, json.name, json.description, json.icon, json.posts, json.likes, json.isLiking);
+        console.log(obj);
         return obj;
     }
-    get id(): Number {
+    get id(): number {
         return this._id;
     }
-
     get name(): string{
         return this._name;
+    }
+    get description(): string{
+        return this._description;
     }
     get icon() {
         return {'background-image': `url(${this._icon})`};
     }
     get posts(): Array<Post>{
         return this._posts;
+    }
+    get likes(): number{
+        return this._likes;
+    }
+    get isLiking(): boolean{
+        return this._isLiking;
     }
 }
--- src/app/post/post.model.ts
+++ src/app/post/post.model.ts
@@ -2,31 +2,33 @@ import { Board } from '../board/board.model';
 
 export class Post {
     constructor(
-        private _id: Number,
+        private _id: number,
         private _title: string,
-        private _user: string,
         private _board: Board,
+        private _user: string,
         private _dateAdded = new Date(),
         private _comments = new Array<string>(),
-        private _likes = new Array<string>()
+        private _likes: number,
+        private _isLiking: boolean
     ){}
 
     static fromJSON(json: any): Post {
-        const obj = new Post(json.id, json.title, json.user, Board.fromJSON(json.board), json.dateAdded, json.comments, json.likes);
+        const obj = new Post(json.id, json.title, Board.fromJSON(json.board), json.user, json.dateAdded, json.comments, json.likes, json.isLiking);
+        console.log(obj);
         return obj;
     }
 
-    get id(): Number{
+    get id(): number{
         return this._id;
     }
     get title(): string{
         return this._title;
     }
-    get user(): string{
-        return this._user;
-    }
     get board(): Board{
         return this._board;
+    }
+    get user(): string{
+        return this._user;
     }
     get dateAdded(): Date{
         return this._dateAdded;
@@ -34,7 +36,10 @@ export class Post {
     get comments(): Array<string>{
         return this._comments;
     }
-    get likes(): Array<string>{
+    get likes(): number{
         return this._likes;
+    }
+    get isLiking(): boolean{
+        return this._isLiking;
     }
 }
